1eea8b2d8844bbd7de629d5a389b1a491da0aa59
CarlosLP18 <carloslopez1122@hotmail.com>
2026-03-21 21:27:47 -0300

    Commit api folder y cambios en CreateCampaign

---
+++ frontend/src/api/campaigns.js
@@ -0,0 +1,32 @@
+const BASE_URL = "https://petcareback-ketp.onrender.com"
+
+export const getCampaigns = async () => {
+  const response = await fetch(`${BASE_URL}/campaigns`, {
+    method: "GET",
+  })
+  if (!response) throw new Error("Error to get all Campaings")
+  return response.json()
+}
+
+export const createCampaign = async (data) => {
+  const response = await fetch(`${BASE_URL}/campaigns`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+
+  if (!response.ok) {
+    const errorText = await response.text()
+    throw new Error(errorText || "Error creating Campaign")
+  }
+
+  return response.json()
+}
+
+export const getCampaingsbyId = async (id) => {
+  const response = await fetch(`${BASE_URL}/campaigns/${id}`)
+  if (!response) throw new Error("Error to get Campaign nro {{id}} ")
+  return response.json()
+}
